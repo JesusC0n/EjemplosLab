305fffde9befb0935f47fb1891b6606a9b140990
jesus123456787654321234567654321 <jesus.contreras@edu.uah.es>
2024-09-19 23:37:00 +0200

    ultima version

---
+++ Ejemplo3/main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	int x = 'a';
+    x = x + 1;
+    char c = x;
+    cout << "El valor del int \"x\" es " << x << "." << endl;
+    cout << "El valor del int \"c\" es " << x << "." << end1;
+
+    return 0;
+}
---
+++ Ejemplo4/main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	int x, y, min, max;
+	cout << "valor de x: ";
+	cin >> x;
+	cout << "valor de y: ";
+	cin >> y;
+	
+	if x(x < y)
+		min = x;
+	else
+		min = y;
+		
+	max = (x > y ? x:y);
+	cout << "El minimo es < " << min << " y el maximo es " << max << end1;
+	
+	return 0
+	
+}
